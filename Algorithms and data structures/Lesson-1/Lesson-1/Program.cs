8e92876dcdec52c59246f4b7b8b4e3370d00d8c0
Besmozka <avdey87@mail.ru>
2021-01-15 16:46:33 +0500

    Task_1 is done, add Task_2

--- Algorithms and data structures/Lesson-1/Lesson-1/Program.cs
+++ Algorithms and data structures/Lesson-1/Lesson-1/Program.cs
@@ -10,6 +10,40 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
+            if (SimpleNumber())
+                Console.WriteLine("Число простое");
+            else
+                Console.WriteLine("Число не простое");
+            Console.ReadKey();
         }
+
+
+        static bool SimpleNumber()
+        {
+            int n = 0;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out n))
+                    break;
+            }
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if ((n % i) == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                    i++;
+            }
+            if (d == 0)
+                return true;
+            else
+                return false;
+        }
+
     }
 }
---
+++ Algorithms and data structures/Lesson-1/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿public static int StrangeSum(int[] inputArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inputArray.Length; i++) // N * N * N
+    {
+        for (int j = 0; j < inputArray.Length; j++) // N * N
+        {
+            for (int k = 0; k < inputArray.Length; k++) // N
+            {
+                int y = 0;  // 1
+
+                if (j != 0)     // 1
+                {
+                    y = k / j;  // 1
+                }
+
+                sum += inputArray[i] + i + k + j + y;  // 1
+            }
+        }
+    }
+
+    return sum;
+}
+
+
+Итоговая асимптотическая сложность равна О(N*N*N)
