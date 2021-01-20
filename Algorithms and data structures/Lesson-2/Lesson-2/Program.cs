cf23476dc82389117a2837d95dc651777efb8e13
Besmozka <avdey87@mail.ru>
2021-01-20 12:42:46 +0500

    Update task 1

--- Algorithms and data structures/Lesson-2/Lesson-2/Program.cs
+++ Algorithms and data structures/Lesson-2/Lesson-2/Program.cs
@@ -12,4 +12,15 @@ namespace Task_1
         {
         }
     }
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
 }
