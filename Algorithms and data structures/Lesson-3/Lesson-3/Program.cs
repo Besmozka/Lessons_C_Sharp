05acc4b6fc2ee1c3f5a66a0345d96b746c77c9c4
Besmozka <avdey87@mail.ru>
2021-01-22 16:48:27 +0500

    Add task

---
+++ Algorithms and data structures/Lesson-3/Lesson-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class PointClass
+    {
+        public int X;
+        public int Y;
+    }
+
+    public struct PointStruct
+    {
+        public int X;
+        public int Y;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayLength = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите длину массива: ");
+                if (int.TryParse(Console.ReadLine(), out arrayLength))
+                {
+                    GetArray(arrayLength);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void GetArray(int arrayLength)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+
+            }
+        }
+    }
+}
