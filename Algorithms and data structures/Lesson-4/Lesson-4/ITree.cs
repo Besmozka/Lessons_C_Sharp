632411ea31ce5106fb81c664ce66a55a71e4e2ee
SergeyAvdeev <avdey87@mail.ru>
2021-01-25 22:40:08 +0500

    Двоичное дерево поиска

---
+++ Algorithms and data structures/Lesson-4/Lesson-4/ITree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_4
+{
+    public interface ITree
+    {
+        int count { get; set; }  // количество элементов в дереве
+        void AddData(int value);
+        void AddNode(Node node);// добавление элемента        
+        void DeleteData(int value);
+        void DeleteNode(Node node); //удаление элемента
+        Node SeachData(int value); //поиск элемента
+        void BalanceTree(); //баланс дерева
+        void DrawTree(); //нарисовать дерево
+    }
+}
---
+++ Algorithms and data structures/Lesson-4/Lesson-4/Tree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_4
+{
+    public class Tree : ITree
+    {
+        public int count { get; set; }
+
+        public Node Root;
+
+        public void AddData(int value)
+        {
+            var newNode = new Node(value);
+            var tmp = Root;
+            if (Root == null)
+                Root = newNode;
+            else
+                while (tmp != null)
+                {
+                    if (value > tmp.Data)
+                    {
+                        if (tmp.Right != null)
+                        {
+                            tmp = tmp.Right;
+                            continue;
+                        }
+                        else
+                        {
+                            tmp.Right = new Node(value);
+                            tmp.Right.Parent = tmp.Parent;
+                        }
+                    }
+                    else 
+                        if (value < tmp.Data)
+                        {
+                            if (tmp.Left != null)
+                            {
+                                tmp = tmp.Left;
+                                continue;
+                            }
+                            else
+                            {
+                                tmp.Left = new Node(value);
+                                tmp.Left.Parent = tmp.Parent;
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("Wrong tree state");                  // Дерево построено неправильно
+                        }
+
+                }
+            count++;
+        }
+
+        public void DeleteData(int value)
+        {
+            var tmp = Root;
+            while (tmp != null)
+            {
+                if (tmp.Data == value)
+                {
+                    tmp.Data = tmp.Right.Data;
+                    tmp = tmp.Right;
+                    DeleteData(tmp.Right.Data);
+                }
+            }
+        }
+
+        public void BalanceTree()
+        {
+
+        }
+
+        public void DrawTree()
+        {
+           
+        }
+
+        public Node SeachData(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
