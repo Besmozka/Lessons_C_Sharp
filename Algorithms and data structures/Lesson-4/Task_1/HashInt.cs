197fed310730ff15d9a79fc9acd7ffada1fc60c6
SergeyAvdeev <avdey87@mail.ru>
2021-01-26 22:14:22 +0500

    Использование Хэша

---
+++ Algorithms and data structures/Lesson-4/Task_1/HashInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class HashInt
+    {
+        public int value { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var hashInt = obj as HashInt;
+
+            if (hashInt == null)
+                return false;
+
+            return value == hashInt.value;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = value?.GetHashCode() ?? 0;            
+            return hashCode;
+        }
+
+    }
+}
