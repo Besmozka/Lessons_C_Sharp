bd01b5208e898eb74469ef6860adaff1e34e5403
SergeyAvdeev <avdey87@mail.ru>
2021-01-28 23:44:24 +0500

    BFS и DFS поиск в дереве

---
+++ Algorithms and data structures/Lesson-5/Lesson-5/IBinaryTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_5
+{
+    public interface IBinaryTree
+    {
+        int count { get; set; }  // количество элементов в дереве
+        void AddData(int value);
+        void DeleteData(int value);
+    }
+}
---
+++ Algorithms and data structures/Lesson-5/Lesson-5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree();
+            var treeSize = 20;
+            var randomInt = new Random();
+            for (int i = 0; i < treeSize; i++)
+            {
+                tree.AddData(randomInt.Next(100));
+            }
+            tree.PrintTree();
+
+            var value = Convert.ToInt32(Console.ReadLine());
+            BFSmethod(tree, value);
+        }
+
+        static Node BFSmethod(BinaryTree binaryTree, int searchValue)
+        {
+            var queue = new Queue();
+            var node = binaryTree.Root;
+            queue.Enqueue(node);
+            Node nodeData;
+            while (true)
+            {
+                if (queue == null)
+                {
+                    return null;
+                }
+                nodeData = (Node)queue.Dequeue();
+                if (nodeData.Data == searchValue)
+                {
+                    Console.WriteLine("Найдено");
+                    return node;
+                }
+                else if (node.Left != null)
+                {
+                    queue.Enqueue(node.Left);
+                    node = node.Left;
+                }
+                else if (node.Right != null)
+                {
+                    queue.Enqueue(node.Right);
+                    node = node.Right;
+                }
+            }
+        }
+
+        static void DFSmethod(BinaryTree binaryTree)
+        {
+
+        }
+    }
+}
