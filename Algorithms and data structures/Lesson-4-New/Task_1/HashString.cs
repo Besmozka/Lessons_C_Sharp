550f7bdf00ccc2653b8710e6ad349e0511b23dda
SergeyAvdeev <avdey87@mail.ru>
2021-02-01 21:24:23 +0500

    Benchmark & BinaryTree

---
+++ Algorithms and data structures/Lesson-4-New/Task_1/HashString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    public class HashString
+    {
+        public string value { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var hashString = obj as HashString;
+
+            if (hashString == null)
+                return false;
+
+            return value == hashString.value;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = value?.GetHashCode() ?? 0;
+            return hashCode;
+        }
+
+    }
+}
