b44fad83751e171ceb098378ff990c9ac43acedc
SergeyAvdeev <avdey87@mail.ru>
2021-02-10 23:02:14 +0500

    add

---
+++ Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson_7_Dynamic_programming_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, columns;
+            while (true)
+            {
+                Console.Write("Введите M: ");
+                if (int.TryParse(Console.ReadLine(), out rows))
+                    break;
+            }
+            while (true)
+            {
+                Console.Write("Введите N: ");
+                if (int.TryParse(Console.ReadLine(), out columns))
+                    break;
+            }
+            var pole = new int[rows, columns];
+
+        }
+
+        public void GetCountExits(int[,] pole)
+        {
+
+        }
+    }
+}
