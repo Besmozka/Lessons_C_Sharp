330633b3224d85bd6919ae89eea9f6642e84d4de
Besmozka <avdey87@mail.ru>
2021-02-11 16:34:18 +0500

    Update Program.cs

--- Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
+++ Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
@@ -4,6 +4,7 @@ namespace Lesson_7_Dynamic_programming_
 {
     class Program
     {
+        public static int[] A = new int[10] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, };
         static void Main(string[] args)
         {
             int rows, columns;
@@ -20,12 +21,25 @@ namespace Lesson_7_Dynamic_programming_
                     break;
             }
             var pole = new int[rows, columns];
+            var f = new Program();
+            f.F(5);
 
         }
 
         public void GetCountExits(int[,] pole)
         {
 
+        }
+
+        int F(int n)
+        {
+            if (A[n] != -1) return A[n];
+            if (n < 2) return 1;
+            else
+            {
+                A[n] = F(n - 1) + F(n - 2);
+                return A[n];
+            }
         }
     }
 }
