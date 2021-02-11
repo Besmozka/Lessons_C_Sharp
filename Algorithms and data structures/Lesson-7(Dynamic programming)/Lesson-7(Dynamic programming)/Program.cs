1fdbcef07babe6c65f3281826d0efc97ce86de7e
SergeyAvdeev <avdey87@mail.ru>
2021-02-11 22:26:14 +0500

    Update Lesson

--- Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
+++ Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
+using BenchmarkDotNet.Attributes;
 
-namespace Lesson_7_Dynamic_programming_
+namespace Lesson_7_Dynamic_programming
 {
     class Program
     {
-        public static int[] A = new int[10] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, };
+
+        public static int[,] buffer;
         static void Main(string[] args)
         {
             int rows, columns;
@@ -20,26 +23,60 @@ namespace Lesson_7_Dynamic_programming_
                 if (int.TryParse(Console.ReadLine(), out columns))
                     break;
             }
-            var pole = new int[rows, columns];
-            var f = new Program();
-            f.F(5);
-
+            buffer = new int[rows, columns];
+            Console.WriteLine(GetCountExitsBufferBlackPoint(rows, columns, 2,1));
+            Console.ReadKey();
         }
 
-        public void GetCountExits(int[,] pole)
+        public static int GetCountExitsBuffer(int rows, int columns)
         {
-
-        }
-
-        int F(int n)
-        {
-            if (A[n] != -1) return A[n];
-            if (n < 2) return 1;
-            else
+            if (rows <= 1 || columns <= 1)
             {
-                A[n] = F(n - 1) + F(n - 2);
-                return A[n];
+                return 1;
             }
+            if (buffer[rows - 1, columns - 1] != 0)
+            {
+                return buffer[rows - 1, columns - 1];
+            }
+
+            var countExits = GetCountExitsBuffer(rows - 1, columns) + GetCountExitsBuffer(rows, columns - 1);
+            buffer[rows - 1, columns - 1] = countExits;
+
+            return buffer[rows - 1, columns - 1];
+        }
+
+        public static int GetCountExits(int rows, int columns)
+        {
+            if (rows <= 1 || columns <= 1)
+            {
+                return 1;
+            }
+
+            return GetCountExits(rows - 1, columns) + GetCountExits(rows, columns - 1);
+        }
+
+        public static int GetCountExitsBufferBlackPoint(int rows, int columns, int xPoint1, int yPoint1)
+        {
+            if (rows <= 1 || columns <= 1)
+            {
+                return 1;
+            }
+            if (buffer[rows - 1, columns - 1] != 0)
+            {
+                return buffer[rows - 1, columns - 1];
+            }
+            if ((rows == xPoint1 && columns == yPoint1))
+            {
+                return 0;
+            }
+            if ((xPoint1 == 1 && yPoint1 <= columns) || (xPoint1 <= rows || yPoint1 == 1))
+            {
+                return 0;
+            }
+              var countExits = GetCountExitsBufferBlackPoint(rows - 1, columns, 2, 1) + GetCountExitsBufferBlackPoint(rows, columns - 1, 2, 1);
+            buffer[rows - 1, columns - 1] = countExits;
+
+            return buffer[rows - 1, columns - 1];
         }
     }
 }
