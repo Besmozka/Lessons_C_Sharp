4ca91e043eb197667634cbd264f56f4e0013f2f6
Besmozka <avdey87@mail.ru>
2021-02-12 11:19:00 +0500

    Search Count Exits (Dynamic Programming)

--- Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
+++ Algorithms and data structures/Lesson-7(Dynamic programming)/Lesson-7(Dynamic programming)/Program.cs
@@ -10,7 +10,7 @@ namespace Lesson_7_Dynamic_programming
         public static int[,] buffer;
         static void Main(string[] args)
         {
-            int rows, columns;
+            int rows, columns, xPoint, yPoint;
             while (true)
             {
                 Console.Write("Введите M: ");
@@ -23,60 +23,98 @@ namespace Lesson_7_Dynamic_programming
                 if (int.TryParse(Console.ReadLine(), out columns))
                     break;
             }
+            while (true)
+            {
+                Console.Write("Введите x точки: ");
+                if (int.TryParse(Console.ReadLine(), out xPoint))
+                    break;
+            }
+            while (true)
+            {
+                Console.Write("Введите y точки: ");
+                if (int.TryParse(Console.ReadLine(), out yPoint))
+                    break;
+            }
             buffer = new int[rows, columns];
-            Console.WriteLine(GetCountExitsBufferBlackPoint(rows, columns, 2,1));
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    buffer[i,j] = -1;
+                }
+            }
+            if (xPoint == 1)
+            {
+                for (int i = yPoint - 1; i < columns - 1; i++)
+                {
+                    buffer[xPoint - 1, i] = 0;
+                }
+            }
+
+            if (yPoint == 1)
+            {
+                for (int i = xPoint - 1; i < rows; i++)
+                {
+                    buffer[i, yPoint - 1] = 0;
+                }
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{buffer[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Количество возможных путей из левого верхнего угла в правый нижний равно (с черными метками): {GetCountExitsBufferBlackPoint(rows, columns, xPoint, yPoint)}");
             Console.ReadKey();
         }
 
-        public static int GetCountExitsBuffer(int rows, int columns)
+        public static int GetCountExitsBuffer(int row, int column)
         {
-            if (rows <= 1 || columns <= 1)
+            if (row <= 1 || column <= 1)
             {
                 return 1;
             }
-            if (buffer[rows - 1, columns - 1] != 0)
+            if (buffer[row - 1, column - 1] > 0)
             {
-                return buffer[rows - 1, columns - 1];
+                return buffer[row - 1, column - 1];
             }
 
-            var countExits = GetCountExitsBuffer(rows - 1, columns) + GetCountExitsBuffer(rows, columns - 1);
-            buffer[rows - 1, columns - 1] = countExits;
+            var countExits = GetCountExitsBuffer(row - 1, column) + GetCountExitsBuffer(row, column - 1);
+            buffer[row - 1, column - 1] = countExits;
 
-            return buffer[rows - 1, columns - 1];
+            return buffer[row - 1, column - 1];
         }
 
-        public static int GetCountExits(int rows, int columns)
+        public static int GetCountExits(int row, int column)
         {
-            if (rows <= 1 || columns <= 1)
+            if (row <= 1 || column <= 1)
             {
                 return 1;
             }
 
-            return GetCountExits(rows - 1, columns) + GetCountExits(rows, columns - 1);
+            return GetCountExits(row - 1, column) + GetCountExits(row, column - 1);
         }
 
-        public static int GetCountExitsBufferBlackPoint(int rows, int columns, int xPoint1, int yPoint1)
+        public static int GetCountExitsBufferBlackPoint(int row, int column, int xPoint, int yPoint)
         {
-            if (rows <= 1 || columns <= 1)
+            if ((row <= 1 || column <= 1) && (buffer[row - 1, column - 1] != 0))
             {
                 return 1;
             }
-            if (buffer[rows - 1, columns - 1] != 0)
+            if (buffer[row - 1, column - 1] > -1)
             {
-                return buffer[rows - 1, columns - 1];
+                return buffer[row - 1, column - 1];
             }
-            if ((rows == xPoint1 && columns == yPoint1))
-            {
-                return 0;
-            }
-            if ((xPoint1 == 1 && yPoint1 <= columns) || (xPoint1 <= rows || yPoint1 == 1))
+            if ((row == xPoint && column == yPoint))
             {
                 return 0;
-            }
-              var countExits = GetCountExitsBufferBlackPoint(rows - 1, columns, 2, 1) + GetCountExitsBufferBlackPoint(rows, columns - 1, 2, 1);
-            buffer[rows - 1, columns - 1] = countExits;
+            }            
+            var countExits = GetCountExitsBufferBlackPoint(row - 1, column, xPoint, yPoint) + GetCountExitsBufferBlackPoint(row, column - 1, xPoint, yPoint);
+            buffer[row - 1, column - 1] = countExits;
 
-            return buffer[rows - 1, columns - 1];
+            return buffer[row - 1, column - 1];
         }
     }
 }
