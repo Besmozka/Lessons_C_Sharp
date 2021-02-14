17c126f034e3995aabf0b77a55bc041b1bf7ce11
SergeyAvdeev <avdey87@mail.ru>
2021-02-14 23:25:55 +0500

    Add lesson

---
+++ Algorithms and data structures/Lesson-8(Busket-sort)/Lesson-8(Busket-sort)/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lesson_8_Busket_sort_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int arrayLenght;
+            while (true)
+            {
+                Console.WriteLine("Введите размерность массива: ");
+                if (int.TryParse(Console.ReadLine(), out arrayLenght))
+                    break;
+                
+            }
+            var array = new int[arrayLenght];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = random.Next(1000);
+            }
+        }
+
+        static int[] BusketSort(int[] array)
+        {
+            int[] minArray = new int[array.Length / 2];
+            int[] maxArray = new int[array.Length - minArray.Length];
+            int maxCount = 0, minCount = 0;
+            if (array == null)
+            {
+                Console.WriteLine("Данный массив не существует");
+                return null;
+            }
+            if (array.Length != 0)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[array.Length/2] >= array[i])
+                    {
+                        maxArray[maxCount] = array[i];
+                        maxCount++;
+                    }
+                    else
+                    {
+                        minArray[minCount] = array[i];
+                        minCount++;
+                    }
+                }
+            }
+            else
+            {
+                return array;
+            }
+            return null;
+        }
+
+        static int[] ExternalSort()
+        {
+            return null;
+        }
+    }
+}
